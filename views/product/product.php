4d3e5b1441ae87ead3a48a2519c55171807158cc
Arriane Alabado <arriane.alabado@sdca.edu.ph>
2024-11-23 18:47:11 +0700

    add to cart completely

--- views/product/product.php
+++ views/product/product.php
@@ -93,7 +93,7 @@ if(isset($_SESSION["success"])){
 
                         <!-- Add to Cart Button -->
                         <div class="d-grid gap-2">
-                            <button type="submit" class="btn btn-primary btn-lg">Add to Cart</button>
+                            <button type="submit" class="btn btn-primary btn-lg" <?php echo ($product["stocks"] <= 0 ? "disabled" : "")?>><?php echo ($product["stocks"] <= 0 ? "Soldout" : "Add to Cart")?></button>
                         </div>
                     
                 </div>
